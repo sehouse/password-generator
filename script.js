73128d3be66bf5d2ffcc1d751c5887aff0ce60fe
sehouse <sehouse@umich.edu>
2020-04-18 17:05:15 -0400

    Rewrote my generation of the random special character to become a function and shortened the code.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var arrayNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var randSpecial = Math.random();
-randSpecial = randSpecial * arraySpecial.length;
-randSpecial = Math.floor(randSpecial);
-console.log(arraySpecial[randSpecial]);
+function getRandomSpecial() {
+  return arraySpecial[Math.floor(Math.random() * arraySpecial.length)];
+}
+
+
+console.log(getRandomSpecial());
 
 var randNumber = Math.random();
 randNumber = randNumber * arrayNumber.length;
