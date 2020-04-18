77d609834072f23f0a7a9fb76485e73f04cc7d3e
sehouse <sehouse@umich.edu>
2020-04-18 17:46:30 -0400

    Created the event listener for my button and gave it confirms and prompts to ass for information about the password. I then used a parse to convert the length input from a string to a number.

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
+var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
 var arraySpecial = ["!", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ";", "#", ":", "@", "~", ",", ".", "/", "<", ">", "?"];
 var arrayNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+generateBtn.addEventListener("click", function () {
+  var passwordLength = prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters and no more than 128 characters.)");
+  var confirmSpecial = confirm("Click OK to confirm including special characters.");
+  var confirmLower = confirm("Click ok to confirm including lowercase characters.");
+  var confirmUpper = confirm("Click ok to confirm including uppercase characters.");
+  passwordLength = parseInt(passwordLength);
+
+});
+
+console.log(password);
+
 function getRandomSpecial() {
   return arraySpecial[Math.floor(Math.random() * arraySpecial.length)];
 }
 
-
 console.log(getRandomSpecial());
 
-var randNumber = Math.random();
-randNumber = randNumber * arrayNumber.length;
-randNumber = Math.floor(randNumber);
-console.log(arrayNumber[randNumber]);
+function getRandomNumber() {
+  return arrayNumber[Math.floor(Math.random() * arrayNumber.length)];
+}
 
-var randLower = Math.random();
-randLower = randLower * arrayLower.length;
-randLower = Math.floor(randLower);
-console.log(arrayLower[randLower]);
+console.log(getRandomNumber());
 
-var randUpper = Math.random();
-randUpper = randUpper * arrayUpper.length;
-randUpper = Math.floor(randUpper);
-console.log(arrayUpper[randUpper]);
+function getRandomLower() {
+  return arrayLower[Math.floor(Math.random() * arrayLower.length)];
+}
+
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return arrayUpper[Math.floor(Math.random() * arrayUpper.length)];
+}
+
+console.log(getRandomUpper());
+
