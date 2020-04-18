2a4c95f29d2722ec5627c15bae12ba00abc2a8f3
sehouse <sehouse@umich.edu>
2020-04-18 16:49:46 -0400

    Creation of variables and processes that log random elements of the arrays to the console log, to check if the random selection logic works.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 
 var arraySpecial = ["!", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ";", "#", ":", "@", "~", ",", ".", "/", "<", ">", "?"];
 var arrayNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var arrayLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var arrayUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var randSpecial = Math.random();
+randSpecial = randSpecial * arraySpecial.length;
+randSpecial = Math.floor(randSpecial);
+console.log(arraySpecial[randSpecial]);
+
+var randNumber = Math.random();
+randNumber = randNumber * arrayNumber.length;
+randNumber = Math.floor(randNumber);
+console.log(arrayNumber[randNumber]);
+
+var randLower = Math.random();
+randLower = randLower * arrayLower.length;
+randLower = Math.floor(randLower);
+console.log(arrayLower[randLower]);
+
+var randUpper = Math.random();
+randUpper = randUpper * arrayUpper.length;
+randUpper = Math.floor(randUpper);
+console.log(arrayUpper[randUpper]);
