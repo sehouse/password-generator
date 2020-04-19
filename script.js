3a49adda7754d4b20349ce0aef296430f4a3187b
sehouse <sehouse@umich.edu>
2020-04-19 13:20:10 -0400

    Made changes to JS code, because I just could not get it to work in the way that I had previously coded it. Starting again and trying to be simpler in the implementation.

--- script.js
+++ script.js
@@ -8,25 +8,18 @@ var arrayNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var passwordLength = prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters and no more than 128 characters.)");
+var confirmSpecial = confirm("Click OK to confirm including special characters.");
+var confirmLower = confirm("Click ok to confirm including lowercase characters.");
+var confirmUpper = confirm("Click ok to confirm including uppercase characters.");
+passwordLength = parseInt(passwordLength);
+
+
 generateBtn.addEventListener("click", function () {
-  var passwordLength = prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters and no more than 128 characters.)");
-  var confirmSpecial = confirm("Click OK to confirm including special characters.");
-  var confirmLower = confirm("Click ok to confirm including lowercase characters.");
-  var confirmUpper = confirm("Click ok to confirm including uppercase characters.");
 
-  passwordLength = parseInt(passwordLength);
-
-  passwordText.innerText = generatePassword(passwordLength, confirmSpecial, confirmLower, confirmUpper)
 });
 
-function generatePassword(passwordLength, confirmSpecial, confirmUpper, confirmLower) {
-  var randomPassword = "";
 
-  var userConfirms = confirmSpecial + confirmUpper + confirmLower;
-  console.log("userConfirms", userConfirms);
-  confirmsArray = [confirmSpecial, confirmUpper, confirmLower];
-  console.log("confirmsArray", confirmsArray);
-}
 function getRandomSpecial() {
   return arraySpecial[Math.floor(Math.random() * arraySpecial.length)];
 }
@@ -48,5 +41,3 @@ console.log(getRandomLower());
 function getRandomUpper() {
   return arrayUpper[Math.floor(Math.random() * arrayUpper.length)];
 }
-
-console.log(getRandomUpper());
