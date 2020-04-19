c9e349cc89eca9f4d4a9d685dd885e88cc6f1448
sehouse <sehouse@umich.edu>
2020-04-19 13:38:29 -0400

    COntinuing a simplification of my code, reworking my logic. My previous attempts were too complex for my level of skill.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //DOM
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
 
 //arrays
 var arraySpecial = ["!", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ";", "#", ":", "@", "~", ",", ".", "/", "<", ">", "?"];
@@ -8,17 +7,27 @@ var arrayNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var passwordLength = prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters and no more than 128 characters.)");
-var confirmSpecial = confirm("Click OK to confirm including special characters.");
-var confirmLower = confirm("Click ok to confirm including lowercase characters.");
-var confirmUpper = confirm("Click ok to confirm including uppercase characters.");
-passwordLength = parseInt(passwordLength);
+generateBtn.addEventListener("click", function (writePassword);
+
+var passwordLength;
+var confirmSpecial;
+var confirmLower;
+var confirmUpper;
 
 
-generateBtn.addEventListener("click", function () {
+function generatePassword() {
 
-});
+  passwordLength = prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters and no more than 128 characters.)");
 
+  passwordLength = parseInt(passwordLength);
+
+  confirmSpecial = confirm("Click OK to confirm including special characters.");
+
+  confirmLower = confirm("Click ok to confirm including lowercase characters.");
+
+  confirmUpper = confirm("Click ok to confirm including uppercase characters.");
+}
+console.log(passwordLength, confirmSpecial, confirmLower, confirmUpper);
 
 function getRandomSpecial() {
   return arraySpecial[Math.floor(Math.random() * arraySpecial.length)];
